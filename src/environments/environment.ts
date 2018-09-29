09d6c92b9a8c4c2dd3873d14ee969c54c980eb35
Jason <chngjn@gmail.com>
2018-09-29 17:51:23 -0400

    Implement service

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  appId: 'df5881543b12cb6f943c95a4562b44fd',
+  baseUrl: 'http://'
 };
