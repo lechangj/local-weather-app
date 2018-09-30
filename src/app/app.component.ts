3262602a879f3dff704e5c00645c37b60f7ea373
Jason <chngjn@gmail.com>
2018-09-30 08:32:11 -0400

    Update to version 6 and apply angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,29 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <mat-toolbar color="primary">
+      <span>LocalCast  Weather</span>
+    </mat-toolbar>
+    <div fxLayoutAlign="center">
+      <div class="mat-caption">Your city, yourforecast,right now!</div>
+    </div>
+    <div fxLayout="row">
+      <div fxFlex></div>
+      <div fxFlex="300px">
+        <mat-card>
+          <mat-card-header class="mat-typography">
+            <mat-card-title><div class="mat-title">Current Weather</div></mat-card-title>
+          </mat-card-header>
+          <mat-card-content>
+            <app-current-weather></app-current-weather>
+          </mat-card-content>
+
+        </mat-card>
+      </div>
+      <div fxFlex></div>
+    </div>
+  `
 })
 export class AppComponent {
   title = 'app';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,32 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { CurrentWeatherComponent } from './current-weather/current-weather.component';
 import { WeatherService } from './weather/weather.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CurrentWeatherComponent
+    CurrentWeatherComponent,
+    SideNavComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
   ],
   providers: [WeatherService],
   bootstrap: [AppComponent]
